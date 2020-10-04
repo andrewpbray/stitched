37bbcbdef80bb85b54bd9bfba309852098f78c28
andrewpbray <andrewbray@berkeley.edu>
2020-10-04 16:33:13 -0700

    use:: format

--- demos/slides_css.R
+++ demos/slides_css.R
@@ -1,5 +1,4 @@
-library(xaringanthemer)
-style_mono_accent(
+xaringanthemer::style_mono_accent(
   base_color = "#1c5253",
   header_font_google = google_font("Josefin Sans"),
   text_font_google   = google_font("Montserrat", "300", "300i"),
