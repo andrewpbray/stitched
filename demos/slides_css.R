67596439fbbdc592d51ee83a795580dfab02de7d
andrewpbray <andrewbray@berkeley.edu>
2020-10-03 18:02:57 -0700

    initial commit

---
+++ demos/slides_css.R
@@ -0,0 +1,7 @@
+library(xaringanthemer)
+style_mono_accent(
+  base_color = "#1c5253",
+  header_font_google = google_font("Josefin Sans"),
+  text_font_google   = google_font("Montserrat", "300", "300i"),
+  code_font_google   = google_font("Fira Mono")
+)
