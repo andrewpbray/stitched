5b8628877d8aa13720b0401c53ba34265c114f4b
andrewpbray <andrewbray@berkeley.edu>
2020-10-07 10:47:05 -0700

    revert

--- demos/slides_css.R
+++ demos/slides_css.R
@@ -1,8 +1,6 @@
-# xaringanthemer::style_mono_accent(
-#   base_color = "#1c5253",
-#   header_font_google = xaringanthemer::google_font("Josefin Sans"),
-#   text_font_google   = xaringanthemer::google_font("Montserrat", "300", "300i"),
-#   code_font_google   = xaringanthemer::google_font("Fira Mono")
-# )
-
-xaringanthemer::style_solarized_light()
+xaringanthemer::style_mono_accent(
+  base_color = "#1c5253",
+  header_font_google = xaringanthemer::google_font("Josefin Sans"),
+  text_font_google   = xaringanthemer::google_font("Montserrat", "300", "300i"),
+  code_font_google   = xaringanthemer::google_font("Fira Mono")
+)
