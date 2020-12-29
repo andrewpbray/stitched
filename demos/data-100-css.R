2fdd4f10f9f275a1700f9310fa99093426da90cc
andrewpbray <andrewbray@berkeley.edu>
2020-12-29 15:38:29 -0800

    initial commit

---
+++ demos/data-100-css.R
@@ -0,0 +1,6 @@
+xaringanthemer::style_mono_accent(
+  base_color = "#1c5253",
+  header_font_google = xaringanthemer::google_font("Roboto"),
+  text_font_google   = xaringanthemer::google_font("Roboto"),
+  code_font_google   = xaringanthemer::google_font("Fira Mono")
+)
