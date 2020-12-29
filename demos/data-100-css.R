d8b764008bdd25c6ea211170aa29153ede62b569
andrewpbray <andrewbray@berkeley.edu>
2020-12-29 15:54:18 -0800

    to light

--- demos/data-100-css.R
+++ demos/data-100-css.R
@@ -1,4 +1,4 @@
-xaringanthemer::style_mono_accent(
+xaringanthemer::style_mono_light(
   header_font_google = xaringanthemer::google_font("Roboto"),
   text_font_google   = xaringanthemer::google_font("Roboto"),
   code_font_google   = xaringanthemer::google_font("Fira Mono")
