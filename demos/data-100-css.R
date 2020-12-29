4848855dee8b2b98535da7f94c00fdb748a8a374
andrewpbray <andrewbray@berkeley.edu>
2020-12-29 15:44:50 -0800

    strip out vase color

--- demos/data-100-css.R
+++ demos/data-100-css.R
@@ -1,5 +1,4 @@
 xaringanthemer::style_mono_accent(
-  base_color = "#1c5253",
   header_font_google = xaringanthemer::google_font("Roboto"),
   text_font_google   = xaringanthemer::google_font("Roboto"),
   code_font_google   = xaringanthemer::google_font("Fira Mono")
